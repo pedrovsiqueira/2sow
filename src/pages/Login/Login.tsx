e2b8b1c0e5b72b3a7afa1a5505f9cab5c889b637
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-20 12:16:00 -0300

    get user from context to login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -18,7 +18,8 @@ interface SignInFormData {
 const Login: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext);
+  const { user, signIn } = useContext(AuthContext);
+  console.log(user)
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
