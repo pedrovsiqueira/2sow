c73ff21f87be426e987ed3b813dc5952169fb380
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-22 17:52:44 -0300

    finish login tests

--- src/__tests__/pages/Login.spec.tsx
+++ src/__tests__/pages/Login.spec.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Login from '../../pages/Login/Login';
 import { render, fireEvent, wait } from '@testing-library/react';
-const mockedHistoryPush = jest.fn();
 
+const mockedHistoryPush = jest.fn();
+const mockedSignIn = jest.fn();
+const mockedAddToast = jest.fn();
 //Mock atribuir valores fictícios para variáveis/funções
 //jest.fn é uma função vazia que serve só para saber se ela foi chamada ou não
+
+jest.mock('../../hooks/toast.tsx', () => {
+  return {
+    useToast: () => ({
+      addToast: mockedAddToast,
+    }),
+  };
+});
 
 jest.mock('react-router-dom', () => {
   return {
@@ -18,12 +28,16 @@ jest.mock('react-router-dom', () => {
 jest.mock('../../hooks/auth.tsx', () => {
   return {
     useAuth: () => ({
-      signIn: jest.fn(),
+      signIn: mockedSignIn,
     }),
   };
 });
 
 describe('Login Page', () => {
+  beforeEach(() => {
+    mockedHistoryPush.mockClear();
+  });
+
   it('should be able to log in', async () => {
     const { getByTestId } = render(<Login />);
 
@@ -45,4 +59,53 @@ describe('Login Page', () => {
     });
   });
 
+  it('should return an error trying to login with invalid credentials', async () => {
+    const { getByTestId } = render(<Login />);
+
+    const emailField = getByTestId('email');
+    const passwordField = getByTestId('password');
+    const buttonElement = getByTestId('login');
+
+    fireEvent.change(emailField, {
+      target: { value: 'not-valid-invalid' },
+    });
+    fireEvent.change(passwordField, {
+      target: { value: 'invalid' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalled();
+    });
+  });
+
+  it('should display an error if login fails', async () => {
+    mockedSignIn.mockImplementation(() => {
+      throw new Error();
+    });
+
+    const { getByTestId } = render(<Login />);
+
+    const emailField = getByTestId('email');
+    const passwordField = getByTestId('password');
+    const buttonElement = getByTestId('login');
+
+    fireEvent.change(emailField, {
+      target: { value: 'pedrosiqueira@gmail.com' },
+    });
+    fireEvent.change(passwordField, {
+      target: { value: '123456' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockedAddToast).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: 'error',
+        })
+      );
+    });
+  });
 });
